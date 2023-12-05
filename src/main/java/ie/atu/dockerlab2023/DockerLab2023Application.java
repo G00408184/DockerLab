a2798c56a9bf7e6c56e382f311decac1f4144e37
Jakub Mamala <g00408184@atu.ie>
2023-12-05 16:15:52 +0000

    DockerLab

---
+++ src/main/java/ie/atu/dockerlab2023/DockerLab2023Application.java
@@ -0,0 +1,13 @@
+package ie.atu.dockerlab2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLab2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerLab2023Application.class, args);
+	}
+
+}
